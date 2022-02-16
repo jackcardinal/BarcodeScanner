48d994ab1dc6e16ade14bcc476d787a811cdd87d
Jack Cardinal <general@jackcardinal.com>
2022-02-15 22:17:10 -0800

    Initial commit after first video

---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,38 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Jack Cardinal on 2/15/22.
+//
+
+import SwiftUI
+
+
+struct BarcodeScannerView: View {
+    
+    @State private var barcodeIsScanned = false
+
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center) {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                Spacer().frame(height: 60)
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                Text("Not Yet Scanned")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct BarcodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
