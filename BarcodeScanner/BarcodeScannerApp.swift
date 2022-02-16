18ec71c39356e0838b13d0ab451b35ba015bde7c
Jack Cardinal <general@jackcardinal.com>
2022-02-15 20:45:52 -0800

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Jack Cardinal on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
