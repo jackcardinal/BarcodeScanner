4b36c9a51d4d488a5d54b1be27e8a49604688a66
Jack Cardinal <general@jackcardinal.com>
2023-01-23 20:47:42 -0800

    Initial commit of app

--- BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -15,7 +15,7 @@ struct BarcodeScannerView: View {
         NavigationView {
             VStack(alignment: .center) {
                 ScannerView(scannedCode: $viewModel.scannedCode , alertItem: $viewModel.alertItem)
-                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    .frame( maxHeight: 300)
                 Spacer().frame(height: 60)
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
@@ -25,14 +25,14 @@ struct BarcodeScannerView: View {
                     .foregroundColor(viewModel.statusTextColor)
                     .padding()
 //                Button{
-//                    isShowingAlert = true
+//                    isShowingAlert = true   
 //                } label: {
 //                    Text("Tap Me")
 //                }
             }
             .navigationTitle("Barcode Scanner")
             .alert(item: $viewModel.alertItem) { alertItem in
-                Alert(title: Text(alertItem.title), message: Text(alertItem.message), dismissButton: alertItem.dismissButton)
+                Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
                 
             }
 //            .alert(isPresented: $isShowingAlert, content: {
--- BarcodeScanner/Utilites/Alert.swift
+++ BarcodeScanner/Utilites/Alert.swift
@@ -10,15 +10,15 @@ import SwiftUI
     
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input", message: "Something wrong with the camera", dismissButton: .default(Text("Okay")))
+    static let invalidDeviceInput = AlertItem(title: Text("Invalid Device Input"), message: Text("Something wrong with the camera"), dismissButton: .default(Text("Okay")))
     
-    static let invalidScannedType = AlertItem(title: "Invalid Scan Type", message: "Only specific barcodes are allowed", dismissButton: .default(Text("Okay")))
+    static let invalidScannedType = AlertItem(title: Text("Invalid Scan Type"), message: Text("Only specific barcodes are allowed"), dismissButton: .default(Text("Okay")))
 }
     
     
